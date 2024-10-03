b712dea3f3c2913a304da768fbb36b8f1358fba3
Vaishnotiwari12 <152400884+Vaishnotiwari12@users.noreply.github.com>
2024-10-04 01:24:37 +0530

    Update 01_variables.js

--- 01_variables.js
+++ 01_variables.js
@@ -1,13 +1,13 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "vaishno@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Lucknow"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
+accountEmail = "vp@ts.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 
