522fd15f5e102f847821609a98b98608f1ef9aaa
Vaishnotiwari12 <152400884+Vaishnotiwari12@users.noreply.github.com>
2024-10-04 19:45:13 +0000

    02_datatypes.js

---
+++ 03_conversionoperations.js
@@ -0,0 +1,72 @@
+ let score=33;
+
+//console.log(typeof score);
+
+//console.log(typeof(score));
+
+let valueInNumber= Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//Nan=>33abc
+// true =>1; false=>0
+
+let isLoggedIn="vaishnav"
+let booleanLoggedIn= Boolean(isLoggedIn);
+//console.log(booleanLoggedIn);
+
+//1=> true; 0=> false;  
+//""=> false;
+//"vaishnav"=> true
+
+let someNumber=33
+
+let stringNumber = String(someNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ---*******operations*******------
+
+let value=3
+let negValue= -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-3);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(3%2);
+
+
+let Str1="vaishnav";
+let Str2=" Tiwari";
+let Str3=Str1+Str2;
+//console.log(Str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2 +"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1 ,num2,num3;
+num1=num2=num3=2+2
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+ 
+
+
+
+
+
+
+
+
+
