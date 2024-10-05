f5e7a5efbc51b9850d6933af89ee7f52917b440a
Vaishnotiwari12 <152400884+Vaishnotiwari12@users.noreply.github.com>
2024-10-05 21:43:12 +0000

    change in location

---
+++ 05_datatypessummary.js
@@ -0,0 +1,29 @@
+
+
+const isLoggedIn = false;
+const outsideTemp =null;
+
+let userEmail;
+
+const id=Symbol('123');
+const anotherId=Symbol('123');
+console.log(id===anotherId);
+
+
+const bigNumber=2143236353231
+
+// reference(non-primitive)
+//Arrays ,objects ,functions
+
+const heros=["shaktimaan","naagraj","doga"];
+let myObj= {
+    name:"vaishno tiwari",
+    age:20,
+
+}
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}
+console.log(typeof heros);
