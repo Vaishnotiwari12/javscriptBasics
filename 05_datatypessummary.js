9ad44d05c43884e37678475844474c9073065f1d
Vaishnotiwari12 <152400884+Vaishnotiwari12@users.noreply.github.com>
2024-10-06 19:26:20 +0000

    Change in Location with notes

--- 05_datatypessummary.js
+++ 05_datatypessummary.js
@@ -47,3 +47,59 @@ console.log(typeof heros);
 //        Function  =>  function
 //        Object  =>  object
 
+
+
+//++++++++++++++++++++++++++++++++++++++//
+
+//stack(primitive) ,heap(non-primitive)
+
+let myName= "vaishno tiwari"
+
+let anotherName="myName"
+
+anotherName="satyam tiwari"
+
+console.log(myName);
+
+console.log(anotherName);
+
+
+let userOne= {
+    email:"vaishnotiwari12@gmail.com",
+    upi:"user@okaxis"
+}
+
+let usertwo=userOne
+usertwo.email="satyamvaishno@gmail.com"
+
+console.log(userOne.email);
+console.log(usertwo.email);
+
+
+
+// **********notes***********1-------------=>
+
+
+// datatypes are based on memory allocation
+// Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+// Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+// JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
+
+//notes ********2*********************------------=>
+
+// There are 2 types of datatype
+// 1. Primitive datatype:-
+//     1.String
+//     2. Number
+//     3. Boolean (true / false)
+//     4. null (iska matlab hai empty or zero nahi hai)
+//     5. undefined (aapne variable declare kardiya hai but usmein aap kya value dalenge aapne decide nahi kiya hai)
+//     6. Symbol (kisi bhi value ko unique banane ke liye use kiya jata hai)
+//     7. BigInt (jo badi values hoti hai oo hum BigInt mein use karte hai)
+
+// 2. Preference datatype (Non Primitive datatype)
+//     1. Array ( [ ] )
+//     2. Object ( { } )
+//     3. Function ( function( ){ } )
