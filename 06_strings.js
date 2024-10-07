6634af27879a93e75a62a4c382087e2b9e3e6459
Vaishnotiwari12 <152400884+Vaishnotiwari12@users.noreply.github.com>
2024-10-07 15:20:45 +0000

    Strings.js

--- 06_strings.js
+++ 06_strings.js
@@ -43,7 +43,7 @@ console.log(url.split('- '));
 
 
 
-//************AAJ KA GYAAN *******--------------------------->>>>
+//************AAJ KA GYAAN *******-----learn these things for interview--------------->>>>
 
 
 // /datatypes are based on memory allocation
