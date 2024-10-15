dc2aab9e6179d1d3b65e7794d2eb2f5a84c41297
Vaishnotiwari12 <152400884+Vaishnotiwari12@users.noreply.github.com>
2024-10-16 00:38:21 +0530

    04_IIFE.js

---
+++ 02_Basic.js/04_IIFE.js
@@ -0,0 +1,19 @@
+//IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+(function chai(){
+       //named IIFE
+       console.log(`DB CONNECTED`);
+       
+})(); //semicolon compulsory hai jisse next code run ho sake  
+
+
+((name) => {
+    console.log(`BD CONNECTED TO ${name}`);
+    
+} )('vaishno')
+
+
+
+ 
+
+
