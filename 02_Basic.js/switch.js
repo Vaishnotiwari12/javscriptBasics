03457e0625dd07f9bf270db10cc86357e355829e
Vaishnotiwari12 <152400884+Vaishnotiwari12@users.noreply.github.com>
2024-10-19 16:09:30 +0530

    switch.js

---
+++ 02_Basic.js/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 2 //or// string bhi le sakte hain 
+switch (month) {
+    case 1:
+        console.log("January"); 
+        break;  // press SHIFT + ALT + DOWNARROW for duplicate
+    case 2:
+        console.log("feb"); 
+        break;  
+    case 3:
+        console.log("March"); 
+        // break;  break ke baad sara code execute 
+        //except defalt 
+    case 4:
+        console.log("April"); 
+        break;  
+
+    default:
+        console.log("defalt case match");
+        
+        break;
+}
+
